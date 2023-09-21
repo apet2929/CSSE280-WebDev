bc1bcd967fe2f0f443e963a0ec19d20a415d6bdc
apet2929 <realandrewpetersen@gmail.com>
2023-09-21 14:46:40 -0400

    Finish follow along 4 but forreal this time

--- public/FollowAlongs/follow-along-4/public/scripts/main.js
+++ public/FollowAlongs/follow-along-4/public/scripts/main.js
@@ -1,13 +1,30 @@
+let getScreenSize = () => {
+    width = window.innerWidth;
+    if(width > 1000){
+        return "Desktop"
+    } else if (width > 760) {
+        return "Laptop"
+    } else if (width > 576) {
+        return "Tablet"
+    } else {
+        return "Mobile"
+    }
+    // less than 500 -> mobile
+    // 500 -> 760 -> tablet
+    // 760 -> 1000 -> laptop
+    // greater than 1000 -> desktop
+}
+
 function screen() {
     width = window.innerWidth;
     document.getElementById("size").innerHTML = `Width : ${width} px`;
-
+    document.getElementById("screen").innerHTML = `${getScreenSize()} 👇🏿`
 }
 
 window.onresize = screen;
 window.onload = screen;
 
-main = () => {
+let main = () => {
     console.log("Ready");
     console.log(window.innerWidth);
     screen();
